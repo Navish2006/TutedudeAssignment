c3d209ece78c07bfd759eafac6604626a55d47d6
Navish2006 <officialnavish2006@gmail.com>
2026-01-18 19:20:07 +0530

    assignment1

--- Assignment1.py
+++ Assignment1.py
@@ -1,1 +1,16 @@
-git pull origin main
+#Task 1: Perform Basic Mathematical Operations
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+add=num1+num2
+sub=num1-num2
+mult=num1*num2
+div=num1/num2
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mult)
+print("Division:",div)
+
+#Task 2: Create a Personalized Greeting
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+print("Hello,",first_name,last_name,"!","Welcome to the Python program.")
